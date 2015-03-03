394193205a6f1b8ff69f97868dda4573ad9f9e99
Ben Liebeskind <bjl786@ada.icmb.utexas.edu>
2015-03-03 10:51:58 -0600

    Added file two_file_difference
    Similar to two_file_intersection except it prints lines in the second file
    that *don't* intersect with the first.

---
+++ two_file_difference.sh
@@ -0,0 +1,5 @@
+# Print all lines in file2 whose 1st column is not in the 1st column of file 1
+# Should not be sensitive to order, as the first files contents are stored in
+# an array
+
+awk 'NR==FNR{a[$1];next}!($1 in a)' $1 $2
