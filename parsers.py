8e6d0f121019fc460261cef2537f06d42e63e613
Ben Liebeskind <bliebeskind@austin.utexas.edu>
2015-04-01 13:30:57 -0500

    Added function eggnog_parser.
    Parses eggNOG .members.txt files. To be used for feeding info into taxon_count.

--- parsers.py
+++ parsers.py
@@ -9,3 +9,18 @@ def phylome_parser(infile):
 			line[-1] = line[-1].split(' ')
 			assert len(line) == len(values)
 			yield dict(zip(values,line))
+			
+def eggnog_parser(infile,header_string="#"):
+	'''Parses an eggnog .members.txt file. Returns generator of 
+	dicationaries for each line.'''
+	values = ("nog","taxonID","prot_name","start","end")
+	with open(infile) as f:
+		if header_string != None: 	# if header string
+			first = f.readline()	# skip header
+			assert first.startswith(header_string),"No header detected"
+		for line in f:
+			line = line.strip().split("\t")
+			taxon,prot = line[1][:line[1].find(".")],line[1][line[1].find(".")+1:]
+			line_list = [line[0],taxon,prot] + line[2:]
+			assert len(line_list) == len(values)
+			yield dict(zip(values,line_list))
