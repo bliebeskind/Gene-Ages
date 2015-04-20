259a299aa61fe630f9ea9ac6c445de6bc12442bd
Ben Liebeskind <bliebeskind@austin.utexas.edu>
2015-04-20 17:27:16 -0500

    Modularized the functionality of taxon_count.
    It now takes advantage of the parsers and functions modules. It should be able
    to handle multiple database types better in the future. Only supports eggNOG
    at the moment.

--- taxon_count.py
+++ taxon_count.py
@@ -2,81 +2,69 @@
 
 import sys
 import pickle
-
-### To Do: modularize so that taxon_count_gen can read in data from
-### other databases.
+import parsers,functions
+from collections import Counter
 
 ## To get the get the number of each group in each NOG.
 ## Groups are: Archaea,Bacteria,Bikont,Bilateria,Mammalia,Metazoa,Unikont,Vertebrata 
 ## Obviously these groups are nested, but "Vertebrate" means a non-mammal 
 ## Vertebrate, Bilateria means a non-vertebrate Bilaterian, etc.
 ##
-## awk 'NR==FNR{a[$2];next}($1 in a)' HumanNOGsDiseases.tsv NOG.members.txt | python taxon_count.py
 
-## Minor problems:
-##	No taxon level for Opisthokonts, i.e. Fungi are grouped with Dicty
+def get_taxonD(path_to_taxa):
+	'''Load taxonD'''
+	with open(path_to_taxa) as p:
+		return pickle.load(p)
 		
+def filter_eggnog(src):
+	'''Read from eggnog_parser and filter out just "nog" and "taxonID"
+	Return generator of tuples'''
+	### Could take advantage of other info from parser.
+	return ((i["nog"],i["taxonID"]) for i in src)
+
+def filter_phylomeDB(src):
+	### Have to turn "ortholog" values into taxon id for lookup
+	pass
+	#raise Exception("Haven't made second taxon lookup yet!!")
+
+def get_stream(infile,db):
+	'''Take database file and return (category,taxonid) tuples from parsers 
+	to feed to flatten()'''
+	db = db.lower()
+	funcsD = {'eggnog': (filter_eggnog, parsers.eggnog_parser),
+			'phylomedb': (filter_phylomeDB, parsers.phylome_parser)}
+	assert db in funcsD, 'Database %s not found' % db
+	funcs = funcsD[db]
+	return funcs[0](funcs[1](infile))
 	
-def taxon_count_gen(handle,taxD,group_set):
-	'''
-	Returns generator yielding a tuple: (KOG, group_counts), where
-	group counts is a dictionary holding the counts for each taxonomic
-	group.
+def taxon_stream(infile,db,taxonD):
+	'''Convert taxonids to taxonomic names using a the dictionary taxonD.
+	Return (group,taxon) generator.'''
+	src = get_stream(infile,db)
+	for i,j in src:
+		yield i,taxonD[j] # could add error handling?
+		
+def flat_stream(infile,db,taxonD):
+	'''Flatten groups. Return generator of tuples (group, (taxon,taxon...))'''
+	unique_categories = []
+	src = taxon_stream(infile,db,taxonD)
+	return (i for i in functions.flatten(src))
 	
-	handle: an opened file like NOG.members.txt, or similar
-	taxa_file: should be eggnogv4.taxonomies.tsv, or similar
-	'''
-	kog = None
-	group_counts = {t:0 for t in group_set}
-	curr_taxIDs = []
-	for line in handle:
-		if line.startswith("#"): # skip header
-			continue
-		line = line.strip().split("\t")
-		curr_kog = line[0].strip()
-		if curr_kog != kog: # new kog, yield and purge
-			if not kog == None: # if not first iteration
-				yield kog, group_counts
-			kog = curr_kog
-			group_counts = {g:0 for g in group_set}
-			curr_taxIDs = []
-		taxID = line[1].split(".")[0]
-		if taxID in curr_taxIDs: # ignore paralogs
-			continue # taxon already found
-		curr_taxIDs.append(taxID)
-		try:
-			group = taxD[taxID]
-			group_counts[group] += 1
-		except KeyError:
-			raise Exception("tax ID %s not found" % taxID)
-	yield kog, group_counts
-			
-def print_taxon_count(handle,taxD,group_set):
-	'''
-	Print output from taxon_count_gen as a tab-delimited file
+def counter_stream(infile,db,taxonD):
+	'''Collect tuples from flat_stream and count occurrences of each taxon id 
+	in the second element. Return generator of tuples (group, {taxon1: 2, taxon2: 39,...})'''
+	for i,j in flat_stream(infile,db,taxonD):
+		yield i, dict(Counter(j))
+		
+def print_taxon_count(infile,db,taxonD):
+	taxon_list = sorted(list(set([i for i in taxonD.itervalues()]))) # unique taxa from taxonD
+	print "\t".join(["group"] + taxon_list)
+	for i,j in counter_stream(infile,db,taxonD):
+		print i,"\t","\t".join(str(j[taxon]) for taxon in taxon_list)
 	
-	handle: an opened file like NOG.members.txt, or similar
-	taxD:
-	group_set:
-	'''
-	is_first = True
-	for i,j in taxon_count_gen(handle,taxD,group_set):
-		if is_first:
-			header = "\t".join(["NOG"] + sorted(j.keys()))
-			yield header
-			is_first = False
-		yield "\t".join([i] + [str(j[taxon]) for taxon in sorted(j.keys())])
 		
 if __name__ == '__main__':
-	path_to_taxa = "/project/LECA/eggNOG/info_files/eggnogv4.taxonomies.p"
-	with open(path_to_taxa) as pickleFile:
-		taxonD,group_set = pickle.load(pickleFile)
-	try:
-		infile = sys.argv[1] # an opened file like NOG.members.txt
-		with open(infile) as f:
-			for i in print_taxon_count(f,taxonD,group_set):
-				print i
-	except IndexError: # reading from stdin
-		infile = sys.stdin
-		for i in print_taxon_count(infile,taxonD,group_set):
-			print i
+	path_to_taxa = "/project/LECA/info_files/taxonomyD.p"
+	infile,dbtype = sys.argv[1],sys.argv[2]
+	taxonD = get_taxonD(path_to_taxa)
+	print_taxon_count(infile,dbtype,taxonD)
