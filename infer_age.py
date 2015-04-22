08fac3ef962372cea53fa37e23bbef382cff5f03
Ben Liebeskind <bliebeskind@austin.utexas.edu>
2015-04-22 16:06:59 -0500

    Renamed infer_ancNOGs --> infer_age. Now uses a tree for inference.
    The downside of this is that it relies on dendropy. But it will be much more
    flexible in the future because of this.

---
+++ infer_age.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python
+
+import dendropy
+import sys
+
+def get_dendropy_tree(tree_source,format='nexus',source_type='file'):
+	'''Read in a tree using dendropy. Tree should have node labels for ancestral nodes that will be
+	used for aging. They should be of format: [&age: "Metazoa"]'''
+	format,source = format.lower(),source_type.lower()
+	tree_funcD = {'string':dendropy.Tree.get_from_string,
+				'file':dendropy.Tree.get_from_path,
+				'stream':dendropy.Tree.get_from_stream}
+	tree = tree_funcD[source_type](tree_source,format,extract_comment_metadata=True)
+	return tree
+				
+def counts_gen(src,tree):
+	'''
+	Read from taxon_count file and yield two-element tuples:
+	(NOG, {Metazoa:10, Bilateria:90...}
+	'''
+	tree_taxa = [str(i) for i in tree.taxon_set]
+	infile_taxa = src.next().strip().split()[1:] # taxa should be columns 2-end
+	assert sorted(tree_taxa) == sorted(infile_taxa), \
+	"Taxa from infile and tree don't match:\n %s\n%s" % (str(tree_taxa),str(infile_taxa))
+	for line in src:
+		line = line.strip().split()
+		count_cols = line[1:]
+		assert len(count_cols) == len(infile_taxa)
+		yield line[0], {i:j for i,j in zip(infile_taxa,count_cols) if j > 0} # skip zero counts
+	
+def age_generator(src,tree_source,format='nexus',source_type='file'):
+	'''Return generator of tuples where i,j are group,age'''
+	tree = get_dendropy_tree(tree_source,format,source_type)
+	for group, count in counts_gen(src,tree):
+		count_node = tree.mrca(taxon_labels=count.keys())
+		yield group, count_node.annotations.get_value("age")
+		
+				
+if __name__ == '__main__':
+	try:
+		infile,tree = sys.argv[1],sys.argv[2]
+		with open(infile) as f:
+			for i,j in age_generator(f,tree,'nexus','file'):
+				print "\t".join(i,j)
+	except IndexError:
+		src = sys.stdin
+		for i,j in age_generator(src,tree,'nexus','file'):
+			print "\t".join(i,j)
+
