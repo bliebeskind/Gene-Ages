68b6c241e06c1bcbe91cb591bbd36bd980386632
Ben Liebeskind <bliebeskind@austin.utexas.edu>
2015-05-14 15:44:16 -0500

    Added Euk+archaea and Euk+bacteria to age output.
    This makes the script less flexible, should maybe think about a way that the
    user can enter the ages they want - but this involves input beyond the tree
    with ages, because Euk+a and Euk+b are both types of "LUCA"

--- infer_age.py
+++ infer_age.py
@@ -24,7 +24,7 @@ def counts_gen(src,tree):
 	"Taxa from infile and tree don't match:\n %s\n%s" % (str(tree_taxa),str(infile_taxa))
 	for line in src:
 		line = line.strip().split()
-		count_cols = line[1:]
+		count_cols = [int(i) for i in line[1:]]
 		assert len(count_cols) == len(infile_taxa)
 		yield line[0], {i:j for i,j in zip(infile_taxa,count_cols) if j > 0} # skip zero counts
 	
@@ -32,8 +32,22 @@ def age_generator(src,tree_source,format='nexus',source_type='file'):
 	'''Return generator of tuples where i,j are group,age'''
 	tree = get_dendropy_tree(tree_source,format,source_type)
 	for group, count in counts_gen(src,tree):
-		count_node = tree.mrca(taxon_labels=count.keys())
-		yield group, count_node.annotations.get_value("age")
+		if len(count.keys()) > 1:
+			count_node = tree.mrca(taxon_labels=count.keys())
+			age = count_node.annotations.get_value("age")
+			if age == "LUCA": # This block reduces the generality, might think about breaking it out
+				if "Bacteria" not in count:# into another function
+					assert "Archaea" in count, "Neither Archea nor Bacteria found in LUCA group: %s" % group
+					yield group, "Euk+A"
+				elif "Archaea" not in count:
+					assert "Bacteria" in count
+					yield group, "Euk+B"
+				else:
+					yield group, age
+			else:
+				yield group, age
+		else: # just one branch
+			yield group, count.keys()[0] # should add error checking if all zeros
 		
 				
 if __name__ == '__main__':
