bfaa07da69474516281994e0781232edef92e419
Ben Liebeskind <bliebeskind@austin.utexas.edu>
2015-10-18 15:23:34 -0500

    Added canned script calc_consistency.

---
+++ CannedScripts/calc_consistency.py
@@ -0,0 +1,21 @@
+from LECA.node_stats import ageConsistency
+from LECA import csv_parser
+import cPickle as pickle
+import sys
+
+### This program will calculate the consistency score for each gene
+### It prints a comma separated stream (gene,conScore)
+
+############# User input #######################
+
+INFILE = "nodeAges_HUMAN.csv"
+
+############ Don't change #######################
+
+with open("NodeDists.p") as f:
+    nodeDistsD = pickle.load(f)
+
+print ",".join(['',"Consistency"])
+	
+for gene,ageD in csv_parser(INFILE):
+    print ",".join([gene,str(ageConsistency(ageD,nodeDistsD))])
