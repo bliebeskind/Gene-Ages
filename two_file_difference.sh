87a3021145bf32f89ab7ba236bc7699bc8b4d003
Ben Liebeskind <bliebeskind@austin.utexas.edu>
2016-02-27 14:07:36 -0600

    deprecated old code

--- two_file_difference.sh
+++
@@ -1,5 +0,0 @@
-# Print all lines in file2 whose 1st column is not in the 1st column of file 1
-# Should not be sensitive to order, as the first files contents are stored in
-# an array
-
-awk 'NR==FNR{a[$1];next}!($1 in a)' $1 $2
